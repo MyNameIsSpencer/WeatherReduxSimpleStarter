00b11f6449f3ffbe55db452a73cece7b76a8f1f2
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-17 01:17:27 -0400

    add prevent default to search bar

---
+++ src/containers/search_bar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({
+      term: e.target.value
+    });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    // We need to go fetch weather data
+  }
+
+  render() {
+    return(
+      <form onSubmit={ this.onFormSubmit } className="input-group">
+        <input
+          placeholder="Get five day forecast of your faovourite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={ this.onInputChange }
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary"> Submit </button>
+        </span>
+      </form>
+    );
+  }
+}
