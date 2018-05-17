d859a64db6b49b976b536daf034384fae3717772
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-17 11:48:07 -0400

    add url wiring to actions index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY = REACT_APP_WEATHER_API_KEY;
+require('dotenv').config();
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+const ROOT_URL = `https://openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
+
+export const FETCH_WEATHER = 'FEATCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
