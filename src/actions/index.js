239281b233afc7af32fb5365dc8a6cbef00a8ec7
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-17 13:31:32 -0400

    api key in env, must manually transfer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-const ROOT_URL = `https://openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
 
 export const FETCH_WEATHER = 'FEATCH_WEATHER';
 
